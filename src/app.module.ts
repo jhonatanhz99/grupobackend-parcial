7400161a8226ac3dfd3cbae028fabbfc80f8551c
Jhonatanhz99 <jhonatansolarte1125408371@gmail.com>
2026-03-18 21:05:17 -0500

    feat: implementación inicial del módulo de planes

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PlansModule } from './modules/plans/plans.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 3306),
+        username: configService.get<string>('DB_USERNAME', 'root'),
+        password: configService.get<string>('DB_PASSWORD', ''),
+        database: configService.get<string>('DB_DATABASE', 'gimnasio'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Solo para desarrollo
+      }),
+      inject: [ConfigService],
+    }),
+    PlansModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
